1225f25c81ba62ed7a070f7df99d7f79439eb10c
Jonathan Cervantes <72222544+jhonnstark@users.noreply.github.com>
2024-07-24 15:29:16 -0600

    uuid header

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
 
 const api = axios.create({ baseURL: 'http://localhost' })
 
 api.defaults.withCredentials = true
 api.defaults.withXSRFToken = true
+
+// Add a request interceptor to include UUID in headers
+api.interceptors.request.use(config => {
+  config.headers['X-Request-ID'] = uuidv4();
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 
 export { api }
